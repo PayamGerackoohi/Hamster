2e6d724e280c3e556c36952464a23e0392628ea4
Payam1991Gr <pgerackoohi@gmail.com>
2024-07-25 14:16:06 +0330

    Puzzle block numbering edited + rabit content overflow is trimmed

--- pages/js/rabbit/content.js
+++ pages/js/rabbit/content.js
@@ -1,1 +1,1 @@
-contentEl.innerHTML = `<h1>Rocky Rabbit</h1><h2>Supersets</h2><div>${cardData.ul} <img src="${cardData.img}"></div><h2>Enigma</h2><div>${enigmaData}</div>`
+contentEl.innerHTML = `<h1>Rocky Rabbit</h1><h2>Supersets</h2><div style="width:fit-content">${cardData.ul} <img src="${cardData.img}"></div><h2>Enigma</h2><div>${enigmaData}</div>`
--- src/rabbit/js/rabbit/content.js
+++ src/rabbit/js/rabbit/content.js
@@ -1,7 +1,7 @@
 contentEl.innerHTML = `
 <h1>Rocky Rabbit</h1>
 <h2>Supersets</h2>
-<div>
+<div style="width:fit-content;">
   ${cardData.ul}
   <img src="${cardData.img}">
 </div>
