aef35d2e8dd03e5624fc41f5917cbe79520f9eb9
Payam1991Gr <pgerackoohi@gmail.com>
2024-07-28 20:18:07 +0330

    rocky rabbit image size trimming + puzzle practice mobile drag issue is resolved.

--- src/rabbit/js/rabbit/content.js
+++ src/rabbit/js/rabbit/content.js
@@ -4,7 +4,7 @@ function makeContent() {
   let content = [];
   content.push('<h1>Rocky Rabbit</h1>');
   content.push('<h2>Supersets</h2>');
-  content.push('<div style="width:fit-content;direction:rtl;">');
+  content.push('<div class="supersets">');
   content.push(`  ${cardData.description}`);
   content.push(`  <img src="${cardData.img}">`);
   content.push('</div>');
